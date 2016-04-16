be8aff687983a989c01b85fda7c30f0c9cffe873
Egor Zhdan <egor.zhdan@gmail.com>
2016-04-16 16:18:32 +0300

    Improved Stack Exchange classes

--- stack_exchange.py
+++ stack_exchange.py
@@ -1,9 +1,15 @@
+import data
+
+
 class Post(object):
     def __init__(self, node):
         self.identifier = node.get("Id")
         self.score = node.get("Score")
         self.owner_id = node.get("OwnerUserId")
         self.body = node.get("Body")
+
+    def get_owner(self):
+        return data.users[self.owner_id]
 
 
 class User(object):
@@ -12,3 +18,8 @@ class User(object):
         self.display_name = node.get("DisplayName")
         self.reputation = node.get("Reputation")
         self.class2_count = 0
+
+    def add_badge(self, node):
+        badge_class = node.get("Class")
+        if badge_class is not None and int(badge_class) == 2:
+            self.class2_count += 1
